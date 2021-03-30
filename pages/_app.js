0ebda1b773a4f76501519513a810996361dc08bc
renatormsilva <renato.brtkm@gmail.com>
2021-03-30 15:19:41 -0400

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_IMAGE}?v=${new Date().getTime()}`}
+        />
+
+        {/* <meta property="og:url" content="https://teste-og-image.vercel.app/" /> */}
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_IMAGE} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="imagem-teste-nuvem" />
+        <meta property="og:description" content="description" />
+        <meta property="fb:app" content="390969538026908" />
+        <meta property="fb:app_id" content="390969538026908" />
+        <meta property="og:updated_time" content={new Date().getTime()} />
+        <meta
+          http-equiv="Cache-Control"
+          content="no-cache, no-store, must-revalidate"
+        />
+        <meta http-equiv="Pragma" content="no-cache" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
